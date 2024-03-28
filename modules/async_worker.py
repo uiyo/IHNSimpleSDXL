4845df2185d0e9f05d3e4e24ba287f05ad6f69a0
uiyo <wuyang802598@ihuman.com>
2024-03-28 16:56:24 +0800

    fix bug

--- modules/async_worker.py
+++ modules/async_worker.py
@@ -322,7 +322,7 @@ def worker():
 
         controlnet_canny_path = None
         controlnet_cpds_path = None
-        controlnet_pose_info = []
+        controlnet_pose_info = [None]
         clip_vision_path, ip_negative_path, ip_adapter_path, ip_adapter_face_path = None, None, None, None
 
         seed = int(image_seed)
